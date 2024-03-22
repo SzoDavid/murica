b43559b0d30b54f52861eaef695a00b875bb4577
Szobonya Dávid <46794488+SzoDavid@users.noreply.github.com>
2024-03-22 02:40:47 +0100

    began token handling

--- murica_bl/Dao/ITokenDao.php
+++ murica_bl/Dao/ITokenDao.php
@@ -2,12 +2,13 @@
 
 namespace murica_bl\Dao;
 
-use murica_bl\Dto\IUser;
-use murica_bl_impl\Dto\User;
+use murica_bl\Dto\IToken;
+use murica_bl_impl\Dto\Token;
+
 
 interface ITokenDao
 {
-    public function findByToken(string $token): array;
-    public function insert(IUser $model): User;
-    public function remove(IUser $model): void;
+    public function findByToken(string $token): Token;
+    public function insert(IToken $model): Token;
+    public function remove(IToken $model): void;
 }
--- murica_bl_impl/Dao/OracleTokenDao.php
+++ murica_bl_impl/Dao/OracleTokenDao.php
@@ -4,14 +4,16 @@ namespace murica_bl_impl\Dao;
 
 use murica_bl\Constants\TableDefinition;
 use murica_bl\Dao\Exceptions\DataAccessException;
-use murica_bl\Dao\IUserDao;
+use murica_bl\Dao\ITokenDao;
+use murica_bl\Dto\IToken;
 use murica_bl\Dto\IUser;
 use murica_bl\Services\ConfigService\IDataSourceConfigService;
 use murica_bl_impl\DataSource\OracleDataSource;
 use murica_bl_impl\Dto\User;
+use murica_bl_impl\Dto\Token;
 use Override;
 
-class OracleUserDao implements IUserDao
+class OracleTokenDao implements ITokenDao
 {
     //region Properties
     private OracleDataSource $dataSource;
@@ -26,54 +28,11 @@ class OracleUserDao implements IUserDao
     }
     //endregion
 
-    //region SELECT
-    /**
-     * @throws DataAccessException
-     */
     #[Override]
-    public function findAll(): array
+    public function findByToken(string $token): Token
     {
-        $res = array();
-
-        $sql = sprintf("SELECT %s AS ID, %s AS NAME, %s AS EMAIL, %s AS PASSWORD, TO_CHAR(%s,'YYYY-MM-DD') AS BIRTH_DATE FROM %s.%s",
-            TableDefinition::USER_TABLE_FIELD_ID,
-            TableDefinition::USER_TABLE_FIELD_NAME,
-            TableDefinition::USER_TABLE_FIELD_EMAIL,
-            TableDefinition::USER_TABLE_FIELD_PASSWORD,
-            TableDefinition::USER_TABLE_FIELD_BIRTH_DATE,
-            $this->configService->getTableOwner(),
-            TableDefinition::USER_TABLE
-        );
-
-        $stmt = oci_parse($this->dataSource->getConnection(), $sql);
-        oci_execute($stmt, OCI_DEFAULT);
-
-        if (!$stmt) {
-            throw new DataAccessException(oci_error($stmt));
-        }
-
-        while (oci_fetch($stmt)) {
-            $res[] = new User(
-                oci_result($stmt, 'ID'),
-                oci_result($stmt, 'NAME'),
-                oci_result($stmt, 'EMAIL'),
-                oci_result($stmt, 'PASSWORD'),
-                oci_result($stmt, 'BIRTH_DATE')
-            );
-        }
-
-        return $res;
-    }
-
-    /**
-     * @throws DataAccessException
-     */
-    #[Override]
-    public function findByCrit(IUser $model): array
-    {
-        $res = array();
-
-        //TODO: fix search
+        //TODO
+        /*$res = array();
 
         $sql = sprintf("SELECT %s AS ID, %s AS NAME, %s AS EMAIL, %s AS PASSWORD, TO_CHAR(%s,'YYYY-MM-DD') AS BIRTH_DATE 
                               FROM %s.%s WHERE ID LIKE :id",
@@ -103,21 +62,17 @@ class OracleUserDao implements IUserDao
                 oci_result($stmt, 'PASSWORD'),
                 oci_result($stmt, 'BIRTH_DATE')
             );
-        }
-
-        return $res;
+        }*/
     }
-    //endregion
-
 
     #[Override]
-    public function insert(IUser $model): User
+    public function insert(IToken $model): Token
     {
-        return new User("YTWK3B", "Szobonya Dávid", "szobonya.david@gmail.com", "asd", "2003-05-22");
+        // TODO: Implement insert() method.
     }
 
     #[Override]
-    public function remove(IUser $model): void
+    public function remove(IToken $model): void
     {
         // TODO: Implement remove() method.
     }
