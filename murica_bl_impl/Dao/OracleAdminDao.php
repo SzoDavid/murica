8fb7e6499787e2fc091c88a0cd796ee5ae618369
Szobonya Dávid <46794488+SzoDavid@users.noreply.github.com>
2024-04-04 14:14:25 +0200

    admin logika

--- murica_bl_impl/Dao/OracleAdminDao.php
+++ murica_bl_impl/Dao/OracleAdminDao.php
@@ -106,13 +106,60 @@ class OracleAdminDao implements IAdminDao {
         return $res;
     }
 
-    /**
-     * @throws NotImplementedException
-     */
     #[Override]
     public function findByCrit(IAdmin $model): array {
-        // TODO: Implement findByCrit() method.
-        throw new NotImplementedException();
+        $res = array();
+        $crits = array();
+
+        $sql = sprintf("SELECT USR.%s AS ID, USR.%s AS NAME, USR.%s AS EMAIL, USR.%s AS PASSWORD, TO_CHAR(USR.%s,'YYYY-MM-DD') AS BIRTH_DATE FROM %s.%s USR, %s.%s ADMIN WHERE USR.%s = ADMIN.%s",
+                       TableDefinition::USER_TABLE_FIELD_ID,
+                       TableDefinition::USER_TABLE_FIELD_NAME,
+                       TableDefinition::USER_TABLE_FIELD_EMAIL,
+                       TableDefinition::USER_TABLE_FIELD_PASSWORD,
+                       TableDefinition::USER_TABLE_FIELD_BIRTH_DATE,
+                       $this->configService->getTableOwner(),
+                       TableDefinition::USER_TABLE,
+                       $this->configService->getTableOwner(),
+                       TableDefinition::ADMIN_TABLE,
+                       TableDefinition::USER_TABLE_FIELD_ID,
+                       TableDefinition::ADMIN_TABLE_FIELD_USER_ID);
+
+        $id = $model->getUser()->getId();
+        $name = $model->getUser()->getName();
+        $email = $model->getUser()->getEmail();
+
+        if (isset($id)) $crits[] = TableDefinition::USER_TABLE_FIELD_ID . " LIKE :id";
+        if (isset($name)) $crits[] = TableDefinition::USER_TABLE_FIELD_NAME . " LIKE :name";
+        if (isset($email)) $crits[] = TableDefinition::USER_TABLE_FIELD_EMAIL . " LIKE :email";
+
+        if (!empty($crits))
+            $sql .= " AND " . implode(" AND ", $crits);
+
+        if (!$stmt = oci_parse($this->dataSource->getConnection(), $sql))
+            throw new DataAccessException(json_encode(oci_error($stmt)));
+
+        if (isset($id) && !oci_bind_by_name($stmt, ':id', $id, -1))
+            throw new DataAccessException('bind id ' . json_encode(oci_error($stmt)));
+        if (isset($name) && !oci_bind_by_name($stmt, ':name', $name, -1))
+            throw new DataAccessException('bind name ' . json_encode(oci_error($stmt)));
+        if (isset($email) && !oci_bind_by_name($stmt, ':email', $email, -1))
+            throw new DataAccessException('bind email ' . json_encode(oci_error($stmt)));
+
+
+        if (!oci_execute($stmt, OCI_DEFAULT))
+            throw new DataAccessException(json_encode(oci_error($stmt)));
+
+        while (oci_fetch($stmt)) {
+            $res[] = new Admin(new User(
+                                   oci_result($stmt, 'ID'),
+                                   oci_result($stmt, 'NAME'),
+                                   oci_result($stmt, 'EMAIL'),
+                                   oci_result($stmt, 'PASSWORD'),
+                                   oci_result($stmt, 'BIRTH_DATE')
+                               ));
+        }
+
+        return $res;
     }
     //endregion
 
