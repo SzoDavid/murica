78e0a2898864f5a22a2ae49bdea1e933a4570360
Szobonya Dávid <46794488+SzoDavid@users.noreply.github.com>
2024-04-08 20:53:03 +0200

    fixes

--- murica_bl/Dao/ITokenDao.php
+++ murica_bl/Dao/ITokenDao.php
@@ -4,18 +4,17 @@ namespace murica_bl\Dao;
 
 use murica_bl\Dao\Exceptions\DataAccessException;
 use murica_bl\Dto\IToken;
-use murica_bl_impl\Dto\Token;
 
 
 interface ITokenDao {
     /**
      * @throws DataAccessException
      */
-    public function findByToken(string $token): Token|false;
+    public function findByToken(string $token): IToken|false;
     /**
      * @throws DataAccessException
      */
-    public function register(string $token, string $userId, int $expirationDate): Token;
+    public function register(string $token, string $userId, int $expirationDate): IToken;
     /**
      * @throws DataAccessException
      */
