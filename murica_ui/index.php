d0c3927ff592daa1eb0985e33b3a57cd483a0569
Szobonya Dávid <46794488+SzoDavid@users.noreply.github.com>
2024-04-17 19:50:19 +0200

    fix2 electic bogaloo

--- murica_ui/index.php
+++ murica_ui/index.php
@@ -2,7 +2,7 @@
 $context = 'index';
 require 'php/header.php';
 ?>
-<main>
+<main id="content">
     <div class="index-container">
         <img src="img/png/murica-oval-index.png" alt="Murica Logo">
         <h1 style="text-align: center;">Welcome to Murica!</h1>
